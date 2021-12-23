8706866a6f7f9e11c89522ea8dcdb8cd97bd21ef
noe <noe.reyes92@yahoo.com>
2021-12-22 19:06:56 -0600

    debugged board - created board using array and a table

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-console.log("js and html files are connected");
+//start game
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM Content Loaded");
+})
 
-const board = document.getElementById('game-board');
-console.log(board);
+let boardArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9" ]; 
 
+//render board
+loadBoard();
 
+//https://www.py4u.net/discuss/1158967
 function loadBoard() {
-    const div1 = document.createElement('div');
-
+    const gameBoard = document.createElement("table");
+    gameBoard.id = "grid-container";
+    let i = 0; //counter
+    for (let r = 0; r < 3; r++) {
+        let row = gameBoard.insertRow(-1); //inserts a new row (<tr>) in a given <table>, and returns a reference to the new row.
+        for (let c = 0; c < 3; c++) {
+            const cell = row.insertCell(-1); //inserts a new cell (<td>) into a table row (<tr>) and returns a reference to the cell.
+            cell.appendChild(document.createTextNode(boardArray[i++]));
+        }
+    }
+    document.body.appendChild(gameBoard);
 }
+
+//player logic
+
+//game ends
