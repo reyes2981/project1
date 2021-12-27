1deb512960db6e663c1a479f29d329447e2c921a
noe <noe.reyes92@yahoo.com>
2021-12-27 09:30:26 -0600

    redrew tic tac toe grid w/ CSS

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
- //start game
+//start game
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM Content Loaded");
+    // let boardArray = [
+    //     ["", "", ""],
+    //     ["", "", ""],
+    //     ["", "", ""]
+    // ];
+
+    // const winningCombos = [
+    //     [0, 1, 2],
+    //     [3, 4, 5],
+    //     [6, 7, 8],
+    //     [0, 3, 6],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [0, 4, 8],
+    //     [2, 4, 6]
+    // ];
+
+    //render board
+    // loadBoard();
+
+    // function loadBoard() {
+    //     const gameBoard = document.createElement("table");
+    //     gameBoard.id = "board-container"
+    //     //  let i = 0; //counter
+    //     for (let r = 0; r < 3; r++) {
+    //         let row = gameBoard.insertRow(-1); //inserts a new row (<tr>) in a given <table>, and returns a reference to the new row.
+    //         for (let c = 0; c < 3; c++) {
+    //             const cell = row.insertCell(-1); //inserts a new cell (<td>) into a table row (<tr>) and returns a reference to the cell.
+    //             cell.appendChild(document.createTextNode(boardArray[r][c]));
+    //         }
+    //     }
+    //     document.body.appendChild(gameBoard);
 
 
-let boardArray = [["1", "2", "3"],
-             ["4", "5", "6"],
-             ["7", "8", "9"]]; 
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log("DOM Content Loaded");
-// })
-
-//render board
- loadBoard();
-
- function loadBoard() {
-     const gameBoard = document.createElement("table");
-     gameBoard.id = "board-container"
-    //  let i = 0; //counter
-     for (let r = 0; r < 3; r++) {
-         let row = gameBoard.insertRow(-1); //inserts a new row (<tr>) in a given <table>, and returns a reference to the new row.
-         for (let c = 0; c < 3; c++) {
-             const cell = row.insertCell(-1); //inserts a new cell (<td>) into a table row (<tr>) and returns a reference to the cell.
-             cell.appendChild(document.createTextNode(boardArray[r][c]));
-         }
-     }
-     document.body.appendChild(gameBoard);
- }
+    // }
 
 
-//player logic
+    //player logic
 
-//game ends
 
+    //game logic
+
+    //game ends
+
+})
 
 //RESOURCES
 //https://www.py4u.net/discuss/1158967
 //https://www.youtube.com/watch?v=GTWrWM1UsnA&t=829s
+//https://stackoverflow.com/questions/58113438/javascript-tic-tac-toe-check-if-someone-won
+//https://stackoverflow.com/questions/53311809/all-possible-combinations-of-a-2d-array-in-javascript
+//https://www.youtube.com/watch?v=B3pmT7Cpi24&t=124s
+//https://www.youtube.com/watch?v=fPew9OI2PnA
+//
+//
